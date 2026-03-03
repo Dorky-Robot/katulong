645771d4d325c61f39c654b28a4b3eb70929562e
Felix Flores <felix@dorkyrobot.com>
2026-03-02 21:54:40 -0500

    formula: update to v0.10.4

--- Formula/katulong.rb
+++ Formula/katulong.rb
@@ -1,8 +1,8 @@
 class Katulong < Formula
   desc "Self-hosted web terminal with remote shell access"
   homepage "https://github.com/dorky-robot/katulong"
-  url "https://github.com/dorky-robot/katulong/archive/refs/tags/v0.10.3.tar.gz"
-  sha256 "1a9f8349a9c411772cc1a84e5d4e812f248a257f05801bd6edb6f6cb72729ea8"
+  url "https://github.com/dorky-robot/katulong/archive/refs/tags/v0.10.4.tar.gz"
+  sha256 "b9ba774259a5c99a26c6cccbfd07776fa69217d4390458e7d756832e424e4c11"
   license "MIT"
 
   depends_on "node"
