d5152fe789706d63b4d22165dfe289b5c9bc4773
Felix Flores <felix@dorkyrobot.com>
2026-03-16 02:48:47 +0000

    formula: update to v0.26.5

--- Formula/katulong.rb
+++ Formula/katulong.rb
@@ -1,8 +1,8 @@
 class Katulong < Formula
   desc "Self-hosted web terminal with tmux sessions and WebAuthn security"
   homepage "https://github.com/Dorky-Robot/katulong"
-  url "https://github.com/Dorky-Robot/katulong/archive/refs/tags/v0.26.4.tar.gz"
-  sha256 "5decc4339ccc912d2686af6fff6b29f8c322a123c86268fbfc475d1cf85b5c04"
+  url "https://github.com/Dorky-Robot/katulong/archive/refs/tags/v0.26.5.tar.gz"
+  sha256 "6182204087ff8f12549cf6faab0cd46b2d7c7c2319d867abf8641046a31d47a4"
   license "MIT"
 
   depends_on "node"
